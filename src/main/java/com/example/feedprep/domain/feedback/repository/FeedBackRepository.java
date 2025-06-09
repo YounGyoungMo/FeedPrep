990a48a2651f15cfc9481479a13134ed3ee0445b
janghwal <jhp337037@gmail.com>
2025-06-09 09:06:05 +0900

    refactor: FeedBackRepository가 feedback을 담당하도록 수정

--- src/main/java/com/example/feedprep/domain/feedback/repository/FeedBackRepository.java
+++ src/main/java/com/example/feedprep/domain/feedback/repository/FeedBackRepository.java
@@ -2,8 +2,10 @@ package com.example.feedprep.domain.feedback.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.feedprep.domain.feedback.entity.FeedBack;
 import com.example.feedprep.domain.feedbackrequestentity.entity.FeedbackRequestEntity;
 
 @Repository
-public interface FeedBackRepository extends JpaRepository<FeedbackRequestEntity, Long> {
+public interface FeedBackRepository extends JpaRepository<FeedBack, Long> {
 }
