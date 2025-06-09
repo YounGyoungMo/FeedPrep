57f34290815d14aac6d6d9e88dc779c966c4efaf
janghwal <jhp337037@gmail.com>
2025-06-09 09:08:29 +0900

    feat: 피드백 리뷰 임시 연관관계 생성

--- src/main/java/com/example/feedprep/domain/feedbackreview/entity/FeedBackReview.java
+++ src/main/java/com/example/feedprep/domain/feedbackreview/entity/FeedBackReview.java
@@ -4,15 +4,26 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import org.springframework.stereotype.Repository;
 import com.example.feedprep.common.entity.BaseTimeEntity;
+import com.example.feedprep.domain.feedback.entity.FeedBack;
+import com.example.feedprep.domain.user.entity.User;
 
 @Getter
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "feedbackreviews")
 public class FeedBackReview extends BaseTimeEntity {
 	@Id
@@ -21,4 +32,14 @@ public class FeedBackReview extends BaseTimeEntity {
 
 	private int Rating;
 	private  String Content;
+
+	@OneToOne
+	@JoinColumn(name = "feedback_id")
+	private FeedBack feedback;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+
 }
