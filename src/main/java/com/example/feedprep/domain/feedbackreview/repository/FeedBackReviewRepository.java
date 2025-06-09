0f2fbbf47388b5fd8a910a6582556ff3bac912ad
janghwal <jhp337037@gmail.com>
2025-06-09 09:10:11 +0900

    feat: JPQL로 튜터 평점 조회 쿼리 작성

--- src/main/java/com/example/feedprep/domain/feedbackreview/repository/FeedBackReviewRepository.java
+++ src/main/java/com/example/feedprep/domain/feedbackreview/repository/FeedBackReviewRepository.java
@@ -1,9 +1,13 @@
 package com.example.feedprep.domain.feedbackreview.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.feedprep.domain.feedbackreview.entity.FeedBackReview;
 
 @Repository
 public interface FeedBackReviewRepository extends JpaRepository<FeedBackReview, Long> {
+	@Query("SELECT AVG(f.Rating) FROM FeedBackReview f WHERE f.user.userId = :userId")
+	Double findAverageRatingByUserId(@Param("userId") Long userId);
 }
