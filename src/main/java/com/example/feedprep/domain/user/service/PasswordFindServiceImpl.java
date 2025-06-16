6cf9138ec5e835e24efd8def9d44e9532f3278ed
soonch67890 <soonch67890@gmail.com>
2025-06-16 16:35:03 +0900

    Feat : 패스워드 찾기 서비스 작성

---
+++ src/main/java/com/example/feedprep/domain/user/service/PasswordFindServiceImpl.java
@@ -0,0 +1,59 @@
+package com.example.feedprep.domain.user.service;
+
+import com.example.feedprep.common.exception.base.CustomException;
+import com.example.feedprep.common.exception.enums.ErrorCode;
+import com.example.feedprep.common.mail.service.MailService;
+import com.example.feedprep.domain.user.dto.request.MailAuthRequestDto;
+import com.example.feedprep.domain.user.dto.request.MailRequestDto;
+import com.example.feedprep.domain.user.entity.User;
+import com.example.feedprep.domain.user.repository.UserRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordFindServiceImpl implements PasswordFindService{
+
+    private final UserRepository userRepository;
+    private final MailService mailService;
+
+    @Override
+    @Transactional
+    public void sendMail(MailRequestDto mailRequestDto) {
+
+        // 회원 확인 여부
+        User user = userRepository.getUserByEmailOrElseThrow(mailRequestDto.getEmail());
+
+        // 인증 번호 발급
+        Long authNumber = mailService.createAuthNumber();
+
+        // 메일 생성
+        MimeMessage mail;
+        try {
+            mail = mailService.createMail(user.getEmail(), authNumber);
+        } catch (MessagingException e) {
+            throw new CustomException(ErrorCode.CREATE_MAIL_FAIL);
+        }
+
+        // redis에 인증번호 저장
+
+        // 이메일 전송 코드
+        mailService.sendMail(mail);
+    }
+
+    @Override
+    @Transactional
+    public void changeLostPassword(MailAuthRequestDto mailAuthRequestDto) {
+
+        // 회원 확인 여부
+        User user = userRepository.getUserByEmailOrElseThrow(mailAuthRequestDto.getEmail());
+
+        // 인증 확인 여부 - redis
+
+        // 비밀번호 변경
+        user.setPassword(mailAuthRequestDto.getNewPassword());
+    }
+}
