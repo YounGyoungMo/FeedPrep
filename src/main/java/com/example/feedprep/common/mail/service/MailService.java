51c872edbbb337c7b6b487720e4d33372a4d10a9
soonch67890 <soonch67890@gmail.com>
2025-06-17 10:38:49 +0900

    Comment : 메일 서비스의 주석 추가

--- src/main/java/com/example/feedprep/common/mail/service/MailService.java
+++ src/main/java/com/example/feedprep/common/mail/service/MailService.java
@@ -5,9 +5,12 @@ import jakarta.mail.internet.MimeMessage;
 
 public interface MailService {
 
+    // 6자리 인증번호 발급
     Long createAuthNumber();
 
+    // 메세지 형식 작성
     MimeMessage createMail(String mail, Long AuthNumber) throws MessagingException;
 
+    // 메세지 전송
     Void sendMail(MimeMessage message);
 }
