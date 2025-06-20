7eba8b941d9dcae0025317659a4ddee12c7af805
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-20 20:24:44 +0900

    feat:  DTO 생성일자 추가

--- src/main/java/com/example/feedprep/domain/notification/dto/response/NotificationResponseDto.java
+++ src/main/java/com/example/feedprep/domain/notification/dto/response/NotificationResponseDto.java
@@ -1,28 +1,28 @@
 package com.example.feedprep.domain.notification.dto.response;
 
 
+import java.time.LocalDateTime;
+
 import lombok.Getter;
 
 import com.example.feedprep.domain.notification.entity.Notification;
 import com.example.feedprep.domain.notification.enums.NotificationType;
 
 @Getter
-public class NotificatinResponseDto {
+public class NotificationResponseDto {
 	private Long id;
-
 	private NotificationType notificationType;
-
 	private String content;
-
 	private String url;
-
 	private boolean isRead;
+	private LocalDateTime createdAt;
 
-	public NotificatinResponseDto(Notification notification) {
+	public NotificationResponseDto(Notification notification) {
 		this.id = notification.getId();
 		this.notificationType = notification.getNotificationType();
 		this.content = notification.getContent();
 		this.url = notification.getUrl();
 		this.isRead = notification.isRead();
+		this.createdAt = notification.getCreatedAt();
 	}
 }
