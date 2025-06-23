ff8802a34c4e5b1269110b651425c007e230a4c6
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-23 16:15:34 +0900

    feat: 알림 삭제 api 업데이트

--- src/main/java/com/example/feedprep/domain/notification/contoller/NotificationController.java
+++ src/main/java/com/example/feedprep/domain/notification/contoller/NotificationController.java
@@ -4,13 +4,19 @@ import java.io.IOException;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import com.example.feedprep.common.response.ApiResponseDto;
 import com.example.feedprep.common.security.annotation.AuthUser;
 import com.example.feedprep.common.sse.repository.EmitterRepository;
+import com.example.feedprep.domain.notification.service.NotificationServiceImpl;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +24,7 @@ import com.example.feedprep.common.sse.repository.EmitterRepository;
 public class NotificationController {
 
 	private final EmitterRepository emitterRepository;
-
+	private final NotificationServiceImpl notificationService;
 
 	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public SseEmitter subscribe(@AuthUser Long userId){
@@ -33,5 +39,10 @@ public class NotificationController {
 
 		// ④ 여기서 클라이언트로 응답 = SSE 연결 시작
 		return emitter;
+	}
+
+	@DeleteMapping("/{notificationId}")
+	public ResponseEntity<ApiResponseDto> deleteNotification(@AuthUser Long userId, @PathVariable Long notificationId){
+		return new ResponseEntity<>(notificationService.deleteNotification(userId, notificationId), HttpStatus.OK);
 	}
 }
