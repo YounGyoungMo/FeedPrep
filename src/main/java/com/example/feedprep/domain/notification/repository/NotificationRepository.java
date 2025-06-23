2f0efc2832bcff21186a5ceb1aa8f3f06353c355
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-23 16:17:02 +0900

    feat:       - 일반 ID 조회 추가       -findByCreatedAtBefore~ List로 변경       - Long getCountByReceiver 명칭 변경

--- src/main/java/com/example/feedprep/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/feedprep/domain/notification/repository/NotificationRepository.java
@@ -1,20 +1,34 @@
 package com.example.feedprep.domain.notification.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import com.example.feedprep.common.exception.base.CustomException;
+import com.example.feedprep.common.exception.enums.ErrorCode;
 import com.example.feedprep.domain.notification.entity.Notification;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-	@Query("SELECT n FROM Notification n where n.receiverId =:receiverId order by n.createdAt DESC LIMIT 10")
-	Optional<Notification> findNotificationByReceiverId(Long receiverId);
+	Optional<Notification> findById(Long id);
 
-	@Query("SELECT COUNT(n) FROM Notification n where n.isRead = false")
-	Long getCount();
+	List<Notification> findByCreatedAtBeforeOrderByCreatedAtAsc(LocalDateTime threshold);
+
+	@Query("SELECT n FROM Notification n where n.receiverId =:receiverId order by n.createdAt DESC LIMIT 10")
+	List<Notification> findNotificationByReceiverId(Long receiverId);
+
+	@Query("SELECT COUNT(n) FROM Notification n WHERE n.receiverId = :receiverId AND n.isRead = false AND n.isStale = true")
+	Long getCountByReceiver(@Param("receiverId") Long receiverId);
+
+
+	default Notification findByIdOrElseThrow(Long notificationId){
+		return findById(notificationId).orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_NOTIFICATION));
+	}
 }
 
 
