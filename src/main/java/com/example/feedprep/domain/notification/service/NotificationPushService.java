abef0d4256104e77a0742e9db7da306c1e8df317
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-26 20:09:44 +0900

    feat: test

--- src/main/java/com/example/feedprep/domain/notification/service/NotificationPushService.java
+++ src/main/java/com/example/feedprep/domain/notification/service/NotificationPushService.java
@@ -1,37 +1,33 @@
-package com.example.feedprep.domain.notification.service;
-
-import java.io.IOException;
-
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-import com.example.feedprep.common.sse.repository.EmitterRepository;
-import com.example.feedprep.domain.notification.entity.Notification;
-import com.example.feedprep.domain.notification.repository.NotificationRepository;
-
-@Service
-@RequiredArgsConstructor
-public class NotificationPushService {
-
-	private final EmitterRepository emitterRepository;
-	private final NotificationRepository notificationRepository;
-
-	public void sendToUser(Long userId, Notification notification){
-		emitterRepository.get(userId).ifPresent(emitter -> {
-			try{
-				emitter.send(SseEmitter.event()
-					.name("notification")
-					.data(notification));
-
-				long unreadCount = notificationRepository.getCountByReceiver(userId);
-				emitter.send(SseEmitter.event()
-					.name("unread-count")
-					.data(unreadCount));
-			}catch (IOException e){
-				emitter.completeWithError(e);
-				emitterRepository.delete(userId);
-			}
-		});
-	}
-}
+// package com.example.feedprep.domain.notification.service;
+//
+// import java.io.IOException;
+//
+// import lombok.RequiredArgsConstructor;
+//
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+// import com.example.feedprep.common.sse.repository.EmitterRepository;
+// import com.example.feedprep.domain.notification.repository.NotificationRepository;
+//
+// @Service
+// @RequiredArgsConstructor
+// public class NotificationPushService {
+//
+// 	private final EmitterRepository emitterRepository;
+// 	private final NotificationRepository notificationRepository;
+//
+// 	public void sendToUser(Long userId){
+// 		emitterRepository.get(userId).ifPresent(emitter -> {
+// 			try{
+// 		    	long unreadCount = notificationRepository.getCountByReceiver(userId);
+// 				emitter.send(SseEmitter.event()
+// 					.name("unread-count")
+// 					.data(unreadCount));
+// 			}catch (IOException e){
+// 				System.out.println(e.toString());
+// 				emitter.completeWithError(e);
+// 				emitterRepository.delete(userId);
+// 			}
+// 		});
+// 	}
+// }
