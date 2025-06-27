04f13f7d4f4694774d2d045893830df7c313df77
janghwal <jhp337037@gmail.com>
2025-06-27 20:42:45 +0900

    test: git actions test

--- src/test/java/com/example/feedprep/FeedPrepApplicationTests.java
+++ src/test/java/com/example/feedprep/FeedPrepApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.feedprep;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FeedPrepApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+// package com.example.feedprep;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class FeedPrepApplicationTests {
+//
+//     @Test
+//     void contextLoads() {
+//     }
+//
+// }
