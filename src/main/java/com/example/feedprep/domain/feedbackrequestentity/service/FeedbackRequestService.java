620de9858749a9a38b81e5d66e3cd88b4b531042
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-27 17:58:09 +0900

    feat: 피드백 요청 수락 파라미터 변경

--- src/main/java/com/example/feedprep/domain/feedbackrequestentity/service/FeedbackRequestService.java
+++ src/main/java/com/example/feedprep/domain/feedbackrequestentity/service/FeedbackRequestService.java
@@ -26,7 +26,7 @@ public interface FeedbackRequestService {
 	);
 
 	// 피드백 요청 단건 상세 조회
-	FeedbackRequestDetailsDto getFeedbackRequest(Long tutorId, Long requestId);
+	FeedbackRequestDetailsDto getFeedbackRequest(Long userId, Long requestId);
 
 	// 피드백 요청 다건 조회 (튜터)
 	List<FeedbackResponseDetailsDto> getFeedbackRequests(Long tutorId, Integer page, Integer size);
@@ -34,11 +34,11 @@ public interface FeedbackRequestService {
 	// 피드백 신청 수정
 	FeedbackRequestEntityResponseDto updateRequest(Long userId, Long feedbackRequestId, FeedbackRequestDto dto);
 
-	// 피드백 신청 수락
-	FeedbackRequestEntityResponseDto acceptRequest(Long userId, Long feedbackRequestId);
-
 	// 피드백 신청 취소
 	FeedbackRequestEntityResponseDto cancelRequest(Long userId, Long feedbackRequestId);
+
+	// 피드백 신청 수락(튜터)
+	FeedbackRequestEntityResponseDto acceptRequest(Long tutorId, Long requestId);
 
 	// 피드백 요청 거절(튜터)
 	FeedbackRequestEntityResponseDto  rejectFeedbackRequest(Long tutorId, Long requestId, Integer rejectNumber,  FeedbackRejectRequestDto dto);
