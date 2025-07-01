64ff52c7a8d3bdbc8ac89bf92f24cbeb590753b3
soonch67890 <soonch67890@gmail.com>
2025-07-01 15:32:46 +0900

    fix : AdminController의 RequestMapping 값 재설정
     유저 기능을 명시화 하기 위한 리팩토링

--- src/main/java/com/example/feedprep/domain/user/controller/AdminController.java
+++ src/main/java/com/example/feedprep/domain/user/controller/AdminController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin/authority")
+@RequestMapping("/users/admin/authority")
 @RequiredArgsConstructor
 public class AdminController {
 
