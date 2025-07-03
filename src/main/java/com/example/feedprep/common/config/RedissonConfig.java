0baba083327c3d316daa42c79cf95db777db81f1
janghwal <jhp337037@gmail.com>
2025-07-03 14:03:19 +0900

    fix: RedissonConfig setAddress 수정

--- src/main/java/com/example/feedprep/common/config/RedissonConfig.java
+++ src/main/java/com/example/feedprep/common/config/RedissonConfig.java
@@ -17,7 +17,7 @@ public class RedissonConfig {
 	private  String setAddress;
 	@Bean
 	public RedissonClient redissonClient(){
-		setAddress ="rediss://" + redisHost + ":6379";
+		setAddress ="redis://" + redisHost + ":6379";
 		Config config = new Config();
 		config.useSingleServer()
 			.setAddress(setAddress)
