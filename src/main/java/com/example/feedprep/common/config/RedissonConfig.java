14356e7e0804a886c4f97c371ed972069229a9c4
janghwal <jhp337037@gmail.com>
2025-07-03 11:44:36 +0900

    fix: 환경 변수 수정

--- src/main/java/com/example/feedprep/common/config/RedissonConfig.java
+++ src/main/java/com/example/feedprep/common/config/RedissonConfig.java
@@ -11,15 +11,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedissonConfig {
 
-    @Value("${spring.data.redis.host}")
+    @Value("${REDIS_URL:localhost}")
 	private String redisHost;
-	@Value("${spring.data.redis.port}")
-	private String redisPort;
 
 	private  String setAddress;
 	@Bean
 	public RedissonClient redissonClient(){
-		setAddress ="redis://" + redisHost + ":" + redisPort;
+		setAddress ="rediss://" + redisHost + ":" + 6379;
 		Config config = new Config();
 		config.useSingleServer()
 			.setAddress(setAddress)
