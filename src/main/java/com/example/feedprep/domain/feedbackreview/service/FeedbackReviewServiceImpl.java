ac0d581a052a1605decc39f0ab9b3bb90b3c33ea
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-07-06 17:53:06 +0900

    feat: 피드백 리뷰 중복 체크 추가

--- src/main/java/com/example/feedprep/domain/feedbackreview/service/FeedbackReviewServiceImpl.java
+++ src/main/java/com/example/feedprep/domain/feedbackreview/service/FeedbackReviewServiceImpl.java
@@ -59,6 +59,10 @@ public class FeedbackReviewServiceImpl implements FeedbackReviewService {
 	@Transactional
 	@Override
 	public FeedbackReviewDetailsDto createReview( Long userId, Long feedbackId, FeedbackReviewRequestDto dto) {
+
+		if(feedBackReviewRepository.existsByFeedbackIdAndUserId(feedbackId, userId)){
+			throw new CustomException(ErrorCode.DUPLICATE_FEEDBACK_REVIEW);
+		}
 		User user = userRepository.findByIdOrElseThrow(userId);
 		if(!user.getRole().equals(UserRole.STUDENT)){
 			throw new CustomException(ErrorCode.UNAUTHORIZED_REQUESTER_ACCESS);
